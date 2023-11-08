167f8d7946a743296e28463318dc6686b5f93a6a
raviranjan202 <mailraviranjan28@gmail.com>
2023-11-08 16:10:40 +0530

    bcrypt added

--- controllers/users.js
+++ controllers/users.js
@@ -1,28 +1,61 @@
-const User = require('../models/users');
-const jwt = require('jsonwebtoken');
-exports.signUp = async (req, res)=>{
-  try{
-    const {name, email, password} = req.body;
-    const newuser = await User.create({name, email, password});
-    res.status(201).json({newuser});
+const User = require("../models/users");
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+exports.signUp = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
 
-  }catch(err){
+    const existingUser = await User.findOne({ email: email });
+
+    if (existingUser) {
+      return res.send("user already exists");
+    }
+
+    const registerUser = new User({
+      name,
+      email,
+      password,
+    });
+
+    const salt = await bcrypt.genSalt(10);
+
+    registerUser.password = await bcrypt.hash(registerUser.password, salt);
+    await registerUser.save();
+    res.status(201).json({ registerUser });
+  } catch (err) {
     console.log(err);
   }
 };
 
-
 exports.signIn = async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
 
-  const { email, password } = req.body;
-
-  const user = await User.findOne({ email: email, password: password });
-  const token = jwt.sign({ userId:user._id, email: email}, 'process.env.Secret_Key', {expiresIn:'1hr'});
-  console.log(token);
-  
-  if (user) {
-      res.status(200).send(token);
-  } else {
-      res.status(400).send("Incorrect Credentials");
+  const user = await User.findOne({ email: email });
+  if (!user) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid email or password" });
   }
-}
+
+  const isPasswordMatch = await bcrypt.compare(password, user.password);
+
+  if (!isPasswordMatch) {
+    return res
+      .status(401)
+      .json({ success: false, message: "Invalid email or password" });
+  }
+
+  const token = jwt.sign(
+    { userId: user._id, email: email },
+    "process.env.Secret_Key",
+    { expiresIn: "1hr" }
+  );
+  console.log(token);
+
+  if (user) {
+    return res.status(200).send(token);
+  } else {
+    res.status(400).send("Incorrect Credentials");
+  }
+};
